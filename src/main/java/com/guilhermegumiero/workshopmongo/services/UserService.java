9a862a27ecafb41858d26558b9b53daf4ce13a40
Gumierow <guilherme.gumiero@outlook.com>
2024-03-10 12:37:22 -0300

    Obtendo um usuario por id

--- src/main/java/com/guilhermegumiero/workshopmongo/services/UserService.java
+++ src/main/java/com/guilhermegumiero/workshopmongo/services/UserService.java
@@ -1,20 +1,27 @@
 package com.guilhermegumiero.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guilhermegumiero.workshopmongo.domain.User;
 import com.guilhermegumiero.workshopmongo.repository.UserRepository;
+import com.guilhermegumiero.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
-	
+
 	@Autowired
 	private UserRepository repo;
 
-	public List<User> findAll(){
+	public List<User> findAll() {
 		return repo.findAll();
+	}
+
+	public User findById(String id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 }
